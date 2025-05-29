e650c5cd2a8334c689933c90334d5172f001f346
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2025-05-29 10:51:26 -0500

    assignment test 1 done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,15 @@
 class MoviesController < ApplicationController
+  def new 
+     @the_movie = Movie.new
+    render template: "movies/new"
+  end 
+
+  def edit
+    id = params.fetch("id")
+    @the_movie = Movie.where( id: id ).first
+    render template: "movies/edit"
+  end
+
   def index
     matching_movies = Movie.all
 
@@ -8,36 +19,35 @@ class MoviesController < ApplicationController
   end
 
   def show
-    the_id = params.fetch("path_id")
+    the_id = params.fetch("id")
 
     matching_movies = Movie.where({ :id => the_id })
 
-    @the_movie = matching_movies[0]
+    @the_movie = matching_movies.first
 
     render({ :template => "movies/show" })
   end
 
   def create
-    the_movie = Movie.new
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
-    the_movie.released = params.fetch("query_released")
+    @the_movie = Movie.new
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.released = params.fetch("query_released", false)
 
-    if the_movie.valid?
-      the_movie.save
+    if @the_movie.valid?
+      @the_movie.save
       redirect_to("/movies", { :notice => "Movie created successfully." })
     else
-      redirect_to("/movies", { :alert => the_movie.errors.full_messages.to_sentence })
+      render template: "movies/new"
     end
   end
-
   def update
-    the_id = params.fetch("path_id")
-    the_movie = Movie.where({ :id => the_id })[0]
+    the_id = params.fetch("id")
+    the_movie = Movie.where({ :id => the_id }).first
 
     the_movie.title = params.fetch("query_title")
     the_movie.description = params.fetch("query_description")
-    the_movie.released = params.fetch("query_released")
+    the_movie.released = params.fetch("query_released", false)
 
     if the_movie.valid?
       the_movie.save
@@ -48,8 +58,8 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    the_id = params.fetch("path_id")
-    the_movie = Movie.where({ :id => the_id })[0]
+    the_id = params.fetch("id")
+    the_movie = Movie.where({ :id => the_id }).first
 
     the_movie.destroy
 
